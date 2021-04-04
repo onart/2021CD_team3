5be3a7dfd9e6f96415dc52c5ce54eddc78b151ea
piuy11 <34086332+piuy11@users.noreply.github.com>
2021-04-04 21:44:10 +0900

    stt api sample

---
+++ stt/stt_sample.py
@@ -0,0 +1,30 @@
+#
+# code sample from https://cloud.google.com/speech-to-text/docs/samples/speech-quickstart?hl=ko
+# authorize from https://cloud.google.com/docs/authentication/getting-started#setting_the_environment_variable
+# to run the code
+#
+
+
+# Imports the Google Cloud client library
+from google.cloud import speech
+
+
+# Instantiates a client
+client = speech.SpeechClient()
+
+# The name of the audio file to transcribe
+gcs_uri = "gs://cloud-samples-data/speech/brooklyn_bridge.raw"
+
+audio = speech.RecognitionAudio(uri=gcs_uri)
+
+config = speech.RecognitionConfig(
+    encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+    sample_rate_hertz=16000,
+    language_code="en-US",
+)
+
+# Detects speech in the audio file
+response = client.recognize(config=config, audio=audio)
+
+for result in response.results:
+    print("Transcript: {}".format(result.alternatives[0].transcript))
